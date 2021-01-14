ec2e37496ecaaac984076a1d6fdb83fe46a1208e
Josh Goldberg <josh@codecademy.com>
2021-01-13 23:05:44 -0500

    Bumped packages and recreated yarn.lock

--- test/base-setup.js
+++ test/base-setup.js
@@ -1,4 +1,5 @@
+// https://github.com/enzymejs/enzyme/issues/2429
+const Adapter = require("@wojtekmaj/enzyme-adapter-react-17");
 const Enzyme = require("enzyme");
-const Adapter = require("enzyme-adapter-react-16");
 
 Enzyme.configure({ adapter: new Adapter() });
